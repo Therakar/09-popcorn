339f7183e97bdb0c0a2e73825865bcc1bd4bed92
Therakar <matteo.pau200@gmail.com>
2024-01-13 21:13:57 +0100

    adds loading state

--- src/App.js
+++ src/App.js
@@ -54,8 +54,9 @@ const KEY = "927548be";
 
 //STRUCTURAL COMPONENT
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const query = "interstellar";
 
@@ -71,11 +72,13 @@ export default function App() {
   //With async function
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
+      setIsLoading(false);
     }
     fetchMovies();
     console.log(movies);
@@ -121,9 +124,7 @@ export default function App() {
 
         */}
 
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMoviesList watched={watched} />
@@ -131,6 +132,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
 
 //STRUCTURAL COMPONENT
